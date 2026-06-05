2819ce52db9c47e873904ae8d6dc71e74bd2e878
cleitonheltonfriday-ui <cleitonheltonfriday@gmail.com>
2026-06-05 00:26:27 -0300

    Atualizar o app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("Mangavult iniciado!");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const biblioteca = document.querySelector(".biblioteca");
+
+    biblioteca.innerHTML = `
+        <div class="card">
+            <div class="capa"></div>
+
+            <div class="info">
+                <h2>Mangavult funcionando</h2>
+                <p>Biblioteca carregada pelo JavaScript</p>
+            </div>
+        </div>
+    `;
+
+});
